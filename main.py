9ac3e8f595078cd9fc446bd2d07c11ec345b2afc
Farhan0016 <103308228+Farhan0016@users.noreply.github.com>
2023-03-21 06:14:43 +0500

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
+#Easy Level
+# password = ""
+# for letter_count in range(0, nr_letters):
+#     password += random.choice(letters)
+#
+# for symbols_count in range(0, nr_symbols):
+#     password += random.choice(symbols)
+#
+# for numbers_count in range(0, nr_numbers):
+#     password += random.choice(numbers)
+#
+# print(f"Here is your passwrod: {password}")
+
+# Hard Level
 password_list = []
 for letter_count in range(0, nr_letters):
     password_list.append(random.choice(letters))
