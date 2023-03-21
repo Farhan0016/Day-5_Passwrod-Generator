d8e04f0651957244c461861530d958b059509f29
Farhan0016 <103308228+Farhan0016@users.noreply.github.com>
2023-03-21 06:12:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 #Password Generator Project
-#https://repl.it/@appbrewery/password-generator-end
-
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -11,20 +9,6 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Easy Level
-# password = ""
-# for letter_count in range(0, nr_letters):
-#     password += random.choice(letters)
-#
-# for symbols_count in range(0, nr_symbols):
-#     password += random.choice(symbols)
-#
-# for numbers_count in range(0, nr_numbers):
-#     password += random.choice(numbers)
-#
-# print(f"Here is your passwrod: {password}")
-
-# Hard Level
 password_list = []
 for letter_count in range(0, nr_letters):
     password_list.append(random.choice(letters))
